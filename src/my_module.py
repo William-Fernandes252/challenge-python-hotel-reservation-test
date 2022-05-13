bbf42a5e88ef48518aa15eb36afb4603d043b08e
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-13 00:41:22 -0300

    Commit test

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,119 @@
-def get_cheapest_hotel(number):   #DO NOT change the function's name
-    cheapest_hotel = "cheapest_hotel_name"
-    return cheapest_hotel
+from dataclasses import dataclass
+import re
+
+
+@dataclass(frozen=True, slots=True, repr=True, init=True)
+class Hotel:
+    name: str
+    taxes: dict
+    rank: int
+
+
+hotels = [
+    Hotel(
+        name='Lakewood',
+        taxes={
+            'Regular': {
+                'mon': 110.00,
+                'tues': 110.00,
+                'wed': 110.00,
+                'thur': 110.00,
+                'fri': 110.00,
+                'sat': 90.00,
+                'sun': 90.00,
+            },
+            'Rewards': {
+                'mon': 80.00,
+                'tues': 80.00,
+                'wed': 80.00,
+                'thur': 80.00,
+                'fri': 80.00,
+                'sat': 80.00,
+                'sun': 80.00,
+            },
+        },
+        rank=3
+    ),
+    Hotel(
+        name='Bridgewood',
+        taxes={
+            'Regular': {
+                'mon': 160.00,
+                'tues': 160.00,
+                'wed': 160.00,
+                'thur': 160.00,
+                'fri': 160.00,
+                'sat': 60.00,
+                'sun': 60.00,
+            },
+            'Rewards': {
+                'mon': 110.00,
+                'tues': 110.00,
+                'wed': 110.00,
+                'thur': 110.00,
+                'fri': 110.00,
+                'sat': 50.00,
+                'sun': 50.00,
+            },
+        },
+        rank=4
+    ),
+    Hotel(
+        name='Ridgewood',
+        taxes={
+            'Regular': {
+                'mon': 220.00,
+                'tues': 220.00,
+                'wed': 220.00,
+                'thur': 220.00,
+                'fri': 220.00,
+                'sat': 150.00,
+                'sun': 150.00,
+            },
+            'Rewards': {
+                'mon': 100.00,
+                'tues': 100.00,
+                'wed': 100.00,
+                'thur': 100.00,
+                'fri': 100.00,
+                'sat': 40.00,
+                'sun': 40.00,
+            },
+        },
+        rank=5
+    ),
+]
+
+
+# DO NOT change the function's name
+def get_cheapest_hotel(reservation: str) -> str:
+    try:
+        client_type = re.search(r'^(\w+):', reservation).group(1)
+    except AttributeError:
+        client_type = 'Regular'  # Default client type
+
+    reservation_weekdays = re.findall(r'\(([a-z]{3,4})\)', reservation)
+
+    for i, hotel in enumerate(hotels):
+        price = 0.00
+        for day in reservation_weekdays:
+            price += hotel.taxes[client_type][day]
+        if i == 0:
+            cheapest_hotel = hotel
+            min_price = price
+        elif price < min_price:
+            cheapest_hotel = hotel
+        elif price == min_price:
+            if hotel.rank > cheapest_hotel.rank:
+                cheapest_hotel = hotel
+
+    return cheapest_hotel.name
+
+
+def main():
+    reservation = 'Rewards: 11May2009(tues), 27Mar2009(wed), 28Mar2009(thurs)'
+    print(get_cheapest_hotel(reservation))
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,17 +1,30 @@
 from unittest import TestCase
 from context import src
-from src.my_module import get_cheapest_hotel
+from src import my_module
+
 
 class MyTest(TestCase):
     def test1(self):
         result = "Lakewood"
-        self.assertEqual(result, get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
+        self.assertEqual(result, my_module.get_cheapest_hotel(
+            "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
 
     def test2(self):
         result = "Bridgewood"
-        self.assertEqual(result, get_cheapest_hotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
+        self.assertEqual(result, my_module.get_cheapest_hotel(
+            "Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
 
     def test3(self):
         result = "Ridgewood"
-        self.assertEqual(result, get_cheapest_hotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
+        self.assertEqual(result, my_module.get_cheapest_hotel(
+            "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
 
+    def test4(self):
+        result = "Lakewood"
+        self.assertEqual(result, my_module.get_cheapest_hotel(
+            "Rewards: 11May2009(wed), 27Mar2009(thur), 28Mar2009(fri)"))
+
+    def test5(self):
+        result = "Lakewood"
+        self.assertEqual(result, my_module.get_cheapest_hotel(
+            "Rewards: 11May2009(tues), 27Mar2009(wed), 28Mar2009(thurs)"))
